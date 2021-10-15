f76c0542b5a182773033e04a4816da5f8a062c3d
TheNameIsGav <spqr.dominus@gmail.com>
2021-10-15 12:19:39 -0500

    Work setup, adding tilemaps and templating levels. No Logic Done

---
+++ Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfo : MonoBehaviour
+{
+    public int levelId;
+    public int peiceCount;
+
+}
